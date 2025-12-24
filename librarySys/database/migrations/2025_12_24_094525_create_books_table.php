ea53b88e04d5312f2cdd233e693892858fa674ce
Christopher <christopher.schembri.j95249@mcast.edu.mt>
2025-12-24 09:49:01 +0000

    added Book Model and Migration now

---
+++ librarySys/database/migrations/2025_12_24_094525_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->string('slug')->unique();          
+            $table->string('isbn')->nullable();        // optional for ISBN for now
+            $table->unsignedSmallInteger('published_year')->nullable();
+
+            // Foreign keys (relationships)
+            $table->foreignId('author_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+
+            $table->boolean('available')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
